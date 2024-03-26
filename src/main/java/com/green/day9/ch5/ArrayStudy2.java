aa36f7f13f8a7f33d65e6bf4a8413b8b22ff73db
17-012 <ckdgus1373@naver.com>
2024-03-26 17:25:53 +0900

    Update

--- src/main/java/com/green/day9/ch5/ArrayStudy2.java
+++ src/main/java/com/green/day9/ch5/ArrayStudy2.java
@@ -1,8 +1,17 @@
 package com.green.day9.ch5;
 
+import java.util.Arrays;
+
 public class ArrayStudy2 {
     public static void main(String[] args) {
-        int[] numArr = new int[100];
-        
+        int[] numArr = new int[10];
+
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = i + 1;
+        }
+
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.println("numArr[" + i + "] = " + numArr[i]);
+        }
     }
 }
---
+++ src/main/java/com/green/day9/ch5/MissionArray1.java
@@ -0,0 +1,12 @@
+package com.green.day9.ch5;
+
+import java.util.Arrays;
+
+public class MissionArray1 {
+    public static void main(String[] args) {
+        int[] numArr = {10, 20, 30, 40};
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.print(numArr[i] + ", ");
+        }
+    }
+}
